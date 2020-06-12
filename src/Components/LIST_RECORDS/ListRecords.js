49feef99fcde3159ab2e68a0be1f6be85fe32762
ashok <ashokprajangam@gmail.com>
2020-06-12 21:20:58 +0530

    Changed styles to header Changed position to fixed in footer so that it becomes responsive styled list records page

--- src/Components/LIST_RECORDS/ListRecords.js
+++ src/Components/LIST_RECORDS/ListRecords.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './listRecords.css'
 
 class ListRecords extends React.Component{
     constructor(props) {
@@ -7,27 +8,27 @@ class ListRecords extends React.Component{
 
     render(){
         return(
-            <table>
+            <table className='list_table'>
                 <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Mobile</th>
-                        <th>Course</th>
-                        <th>Mark</th>
-                        <th>Action</th>
+                    <tr className="list_row">
+                        <th className='list_head'>Name</th>
+                        <th className='list_head'>Mobile</th>
+                        <th className='list_head'>Course</th>
+                        <th className='list_head'>Mark</th>
+                        <th className='list_head'>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {this.props.list_data.map((record)=>{
                         return (
                             <tr key={record.id}>
-                                <td>{record.name}</td>
-                                <td>{record.mobile}</td>
-                                <td>{record.course}</td>
-                                <td>{record.mark}</td>
-                                <td>
-                                    <button value={record.id} onClick={this.props.onclick}>Edit</button>
-                                    <button value={record.id} onClick={this.props.onDelete}>Delete</button>
+                                <td className='list_data'>{record.name}</td>
+                                <td className='list_data'>{record.mobile}</td>
+                                <td className='list_data'>{record.course}</td>
+                                <td className='list_data'>{record.mark}</td>
+                                <td className='list_data'>
+                                    <button id="edit_button" value={record.id} onClick={this.props.onclick}>Edit</button>
+                                    <button id="delete_button" value={record.id} onClick={this.props.onDelete}>Delete</button>
                                 </td>
                             </tr>
                         )
